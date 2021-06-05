df8b5db60d39a535a31a87a79f745b3b1e71a782
elvin <cgc.elvin@gmail.com>
2021-06-05 17:50:11 +0800

    first commit

---
+++ UartHexCmdClientDemo/UartHexCmdClientDemo.ino
@@ -0,0 +1,49 @@
+/**
+    @file: UartHexCmdClientDemo
+    @author: guncung
+    @date: 2021-06-05
+    @brief: Send a hex cmd via serial, and Arduino returns you a responce data.
+*/
+
+#include "string.h"
+
+/* define recv_cmd and resp_data by yourself */
+uint8_t recv_cmd[] = {0x01, 0x04, 0x00, 0x00, 0x00, 0x08, 0xf1, 0xcc};
+uint8_t resp_data[] = {0x01, 0x04, 0x10, 0x03, 0x34, 0x03, 0x32, 0x03, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x9b, 0x83};
+
+uint8_t recv_data[sizeof(recv_cmd)] = {}; // store incoming serial byte
+int index = 0;
+bool flag = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(20);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available()) {
+    uint8_t b = Serial.read();
+    recv_data[index] = b; // store the incoming byte to recv_data
+    index++;
+  }
+  
+  // compare recv_data with recv_cmd
+  if (index == sizeof(recv_cmd)) {
+    for (int i = 0; i < sizeof(recv_data); i++) {
+      flag = true;
+      if (recv_data[i] != recv_cmd[i]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      Serial.write(resp_data, sizeof(resp_data));
+    }
+  }
+
+  delay(50); // necessary delay
+  memset(recv_data, 0, sizeof(recv_data)); // reset the array
+  index = 0; // reset index
+}
